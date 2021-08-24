6df28a320b0372b126b5c363d6959c398ed5a9bf
aadhar gaur <aadhar41@gmail.com>
2021-08-24 15:53:21 +0530

    Admin Stats Templating

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,83 +3,13 @@
 @section('content')
 <!-- Main content -->
 <section class="content">
+
     <div class="container-fluid">
         <!-- Small boxes (Stat box) -->
-        <div class="row">
-            <div class="col-lg-3 col-6">
-                <!-- small box -->
-                <div class="small-box bg-warning">
-                    <div class="inner">
-                        <h3><?php echo $states; ?></h3>
-                        <p>Active States</p>
-                    </div>
-                    <div class="icon">
-                        <i class="fas fa-map-signs"></i>
-                    </div>
-                    <a href="{{ route('admin.state.list') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                </div>
-            </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-6">
-                <!-- small box -->
-                <div class="small-box bg-success">
-                    <div class="inner">
-                        <h3><?php echo $cities; ?></h3>
-                        <p>Active Cities</p>
-                    </div>
-                    <div class="icon">
-                        <i class="fas fa-map-marked-alt"></i>
-                    </div>
-                    <a href="javascript:void(0);" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                </div>
-            </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-6">
-                <!-- small box -->
-                <div class="small-box bg-info">
-                    <div class="inner">
-                        <h3><?php echo $suburbs; ?></h3>
-                        <p>Active Suburbs</p>
-                    </div>
-                    <div class="icon">
-                        <i class="fas fa-directions"></i>
-                    </div>
-                    <a href="javascript:void(0);" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                </div>
-            </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-6">
-                <!-- small box -->
-                <div class="small-box bg-danger">
-                    <div class="inner">
-                        <h3><?php echo $jobtypes; ?></h3>
-                        <p>Active JobTypes</p>
-                    </div>
-                    <div class="icon">
-                        <i class="fas fa-user"></i>
-                    </div>
-                    <a href="{{ route('admin.jobtype.list') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                </div>
-            </div>
-
-            <div class="col-lg-3 col-6">
-                <!-- small box -->
-                <div class="small-box bg-primary">
-                    <div class="inner">
-                        <h3><?php echo $professions; ?></h3>
-                        <p>Active Professions</p>
-                    </div>
-                    <div class="icon">
-                        <i class="fas fa-user-tie"></i>
-                    </div>
-                    <a href="{{ route('admin.profession.list') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                </div>
-            </div>
-            <!-- ./col -->
-        </div>
-        <!-- /.row -->
+        @include('partials._adminStats')
 
     </div><!-- /.container-fluid -->
+
 </section>
 <!-- /.content -->
 @endsection
