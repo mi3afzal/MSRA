efe238da7d8f5a2cb64c420b58ac2f7c197dccb3
aadhar gaur <aadhar41@gmail.com>
2021-10-12 11:31:59 +0530

    Update listener for only login url

--- app/Listeners/storeUserLoginHistory.php
+++ app/Listeners/storeUserLoginHistory.php
@@ -33,14 +33,28 @@ class storeUserLoginHistory
         $userinfo = $event->user;
         if (isset($userinfo)) {
             $today = date("Y-m-d");
+            $main = DB::table('login_histories')->where(['name' => $userinfo->name, 'user_id' => $userinfo->id, 'server_name' => $_SERVER['SERVER_NAME'], 'http_user_agent' => $_SERVER['HTTP_USER_AGENT'], 'email' => $userinfo->email, 'role' => $userinfo->role])->where("created_at", "like", '%' . $today . '%')->get();
             $users = DB::table('login_histories')->where(['name' => $userinfo->name, 'user_id' => $userinfo->id, 'server_name' => $_SERVER['SERVER_NAME'], 'http_referer' => $_SERVER['HTTP_REFERER'], 'http_user_agent' => $_SERVER['HTTP_USER_AGENT'], 'email' => $userinfo->email, 'role' => $userinfo->role])->where("created_at", "like", '%' . $today . '%')->get();
-            if (count($users) <= env("LOGIN_HISTORY_ROW", 2)) {
+
+            if (count($main) == 0) {
                 $saveHistory = DB::table('login_histories')->insert(
                     ['name' => $userinfo->name, 'user_id' => $userinfo->id, 'server_name' => $_SERVER['SERVER_NAME'], 'http_referer' => $_SERVER['HTTP_REFERER'], 'http_user_agent' => $_SERVER['HTTP_USER_AGENT'], 'email' => $userinfo->email, 'role' => $userinfo->role, 'created_at' => $current_timestamp, 'updated_at' => $current_timestamp]
                 );
                 return $saveHistory;
             } else {
-                return "";
+                if (count($users) <= env("LOGIN_HISTORY_ROW", 2)) {
+                    $row = DB::table('login_histories')->where(['name' => $userinfo->name, 'user_id' => $userinfo->id, 'server_name' => $_SERVER['SERVER_NAME'], 'http_referer' => $_SERVER['HTTP_REFERER'], 'http_user_agent' => $_SERVER['HTTP_USER_AGENT'], 'email' => $userinfo->email, 'role' => $userinfo->role])->where("created_at", "like", '%' . $today . '%')->first();
+                    if (!empty($row->http_referer)) {
+                        if ((strpos($row->http_referer, 'admin/dashboard') !== false) || (strpos($row->http_referer, 'login') !== false)) {
+                            $saveHistory = DB::table('login_histories')->insert(
+                                ['name' => $userinfo->name, 'user_id' => $userinfo->id, 'server_name' => $_SERVER['SERVER_NAME'], 'http_referer' => $_SERVER['HTTP_REFERER'], 'http_user_agent' => $_SERVER['HTTP_USER_AGENT'], 'email' => $userinfo->email, 'role' => $userinfo->role, 'created_at' => $current_timestamp, 'updated_at' => $current_timestamp]
+                            );
+                            return $saveHistory;
+                        }
+                    }
+                } else {
+                    return "";
+                }
             }
         }
         return "";
